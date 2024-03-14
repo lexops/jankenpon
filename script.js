086eddcb42b776fef2ce33bab64c4cfa78c3b233
Alex Silva <134613820+lexops@users.noreply.github.com>
2024-03-14 09:14:49 -0300

    Add playRound function

--- script.js
+++ script.js
@@ -2,13 +2,32 @@ function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3)
   switch (randomNumber) {
     case 0:
-      return 'Rock'
+      return 'rock'
     case 1:
-      return 'Paper'
+      return 'paper'
     case 2:
-      return 'Scissors'
+      return 'scissors'
   }
 }
 
 // console.log(getComputerChoice())
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `Computer chose ${computerSelection}.\nIt's a tie!`
+  }
+
+  if (playerSelection === 'rock' && computerSelection === 'scissors'
+    || playerSelection === 'paper' && computerSelection === 'rock'
+    || playerSelection === 'scissors' && computerSelection === 'paper') {
+
+    return `Computer chose ${computerSelection}.\nYou win!`
+  }
+
+  return `Computer chose ${computerSelection}.\nYou lose!`
+}
+
+const playerSelection = "pApEr".toLowerCase()
+const computerSelection = getComputerChoice()
+
+console.log(playRound(playerSelection, computerSelection))
